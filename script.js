fc50f596c74761e092e1fae0e26f192500bb630a
Hafiz Taha Siraj <Pikachugaming899@gmail.com>
2024-08-25 12:43:43 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+alert("Welcome to our website!");
+alert("Hello, have a great day!");
+alert("Welcome to our website.\nEnjoy your visit!");
+alert("Welcome!");
+alert("We hope you enjoy your stay.");
+alert("Have a great day!");
+
+var username;
+var myName = "Your Full Name";
+
+var message = "Hello World";
+alert(message);
+
+var name = "John Doe";
+var age = "21";
+var course = "JavaScript Basics";
+alert("Name: " + name);
+alert("Age: " + age);
+alert("Course: " + course);
+
+var age = 25;
+alert("Your age is " + age);
+
+var visitCount = 5;
+alert("You have visited this site " + visitCount + " times");
+
+var birthYear = 1995;
+alert("Your birth year is " + birthYear);
+
+var birthYear = 1995;
+alert("Your birth year is " + birthYear);
+
+var visitorName = "John Doe";
+var productTitle = "T-shirt";
+var quantity = 5;
+alert(visitorName + " ordered " + quantity + " " + productTitle + "(s) on XYZ Clothing store");
+
+
+var difference = num1 - num2;
+var product = num1 * num2;
+var quotient = num1 / num2;
+var remainder = num1 % num2;
+
+alert("Difference: " + difference);
+alert("Product: " + product);
+alert("Quotient: " + quotient);
+alert("Remainder: " + remainder);
